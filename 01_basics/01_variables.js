752c720ddf0850629724d727ccd0ead4faec79de
Rohit Narayani <rohitnarayani2005@gmail.com>
2026-05-16 10:15:12 +0000

    Variable and Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const PI = 3.14;
+let name = "Rohit";
+var c = 3;
+age = 22;
+{
+    c = 5
+    console.log(c);
+}
+
+console.table([PI,name,c,age]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";// treat all js code as newer version
+
+const integerData = 20000;
+let stringType = "Hello";
+let booleanType = true;
+let name;
+let temperature = null;
+
+// alert(3+3)// we are in nodejs , not in browser 
+
+/*
+    number => 2 to power 53
+    bigint
+    symbol => unique
+    undefined
+    null => standalone value
+*/
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
