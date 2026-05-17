82a4de8c810b1e15e1b0e293bc3f314c55ba6551
Rohit Narayani <rohitnarayani2005@gmail.com>
2026-05-17 10:44:41 +0000

    Datatypes comparison and summary

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -16,5 +16,34 @@ let temperature = null;
     null => standalone value
 */
 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+// Categories of data type
+
+// Primitive type: Number , String , Bigint , boolean , null , undefined , Symbol
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+// const bigNumber = 545456486887878778
+// console.log(typeof bigNumber);
+
+// Reference(Non primitive) type: Array , Object , Functions
+
+const heros = ['R','o','h','i','t']
+console.log(typeof heros);
+
+let myObject = {
+    name: "Rohit",
+    age : 30
+}
+
+console.log(typeof myObject);
+
+let myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction);
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log("2" >1);
+// console.log("20"<12);
+// console.log("20"<=12);
+// console.log("20">=12);
+// console.log("2a"!=2);
+
+// console.log("30"<null);
+// console.log("20a">null);
+// console.log(null < undefined);
+// console.log(null == undefined);
+// console.log(null > undefined);
+// console.log(undefined >= null);
+
+// console.log(undefined === null);
+// console.log("22" === 22);
+// console.log(22.0 === 22);
+
+console.log(true == null);
+console.log(true == undefined);
+console.log(true == false);
+console.log(null <= true);
+console.log(null != false);
