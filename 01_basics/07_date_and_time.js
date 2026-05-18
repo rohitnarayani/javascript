0d3dd16ef661402ad4b9ba5606fc6ef3b7c7e0ee
Rohit Narayani <rohitnarayani2005@gmail.com>
2026-05-18 11:46:43 +0000

    Dates in js

---
+++ 01_basics/07_date_and_time.js
@@ -0,0 +1,23 @@
+let date = new Date();
+// console.log(date)
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toLocaleString());
+
+// let myDate = new Date(2028,0,2);
+// let myDate2 = new Date(2030,2,29,3,40);
+// console.log(myDate);
+// console.log(myDate2);
+
+let newDate = new Date("2026-5-18")
+// console.log(newDate);
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(newDate.getTime());
+
+// console.log(Math.floor(((Date.now()/1000)/1000)/1000));
+
+console.log(newDate.getDay());
+console.log(newDate.getMonth()+1);
