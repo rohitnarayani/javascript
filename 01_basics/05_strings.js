bc7d2999255e2c2c05cfb09122f22e49f5c200be
Rohit Narayani <rohitnarayani2005@gmail.com>
2026-05-18 09:06:49 +0000

    String methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+let word1 = "standard"
+let word2 = "concatinate"
+
+// console.log(`This is a ${word1} way to ${word2} two strings.This is a string interpolation`)
+let name = new String("               hansaben bharatbhai parmar               ")
+
+// console.log(name[3])
+// console.log(name.__proto__.toUpperCase());
+
+// console.log(name.big());
+// console.log(name.blink());
+
+// console.log(name.concat(" "," Khakhra wada"));
+
+// console.log(name.bold());
+// console.log(name.charAt(10));
+// console.log(name.endsWith("parmar"));
+// console.log(name.fontcolor("red"));
+// console.log(name.lastIndexOf("par"));
+// console.log(name.length);
+// console.log(name.link());
+// console.log(name.match("[a-z]"));
+// console.log(name.normalize());
+// console.log(name.replace("hansa","champa"));
+// console.log(name.split("b"));
+// console.log(name.substr(0,5));
+// console.log(name.substring(0,5));
+// console.log(name.toUpperCase());
+// console.log(name);
+// console.log(name.trim());
+// console.log(name.includes("bhai"));
+console.log(name.valueOf("hansa"));
+
