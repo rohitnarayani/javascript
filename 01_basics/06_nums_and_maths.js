4af74672b9284b9eb8476d2973ad62772179d479
Rohit Narayani <rohitnarayani2005@gmail.com>
2026-05-18 10:50:41 +0000

    Numbers and Maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,31 @@
+// let balance = new Number(2000);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+
+// let number = 342.58054
+// console.log(number.toPrecision(2));
+
+// const thousands = 1000000;
+// console.log(thousands.toLocaleString("en-IN"));
+
+// ++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++
+
+const decimalNumber = 93.545328;
+
+// console.log(Math.PI);
+// console.log(Math.floor(decimalNumber))
+// console.log(Math.ceil(decimalNumber))
+// console.log(Math.round(decimalNumber));
+// console.log(Math.sqrt(196));
+// console.log(Math.pow(2,5))
+// console.log(Math.abs(-33));
+// console.log(Math.min(1,2,4,5,56,5,43,8))
+// console.log(Math.max(1,2,4,5,56,5,43,8))
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
