57cee29e674e46d172b2b12b96f3f3801a77762f
Rohit Narayani <rohitnarayani2005@gmail.com>
2026-05-19 06:51:14 +0000

    Objects part 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,59 @@
+// const user = new Object();
+// console.log(typeof user);
+
+const bankAccount = {}
+bankAccount.accountHolderName = "Walter white"
+bankAccount.branch = "Karamsad"
+bankAccount.balance = 30000
+bankAccount.accountType = "saving"
+// console.log(bankAccount);
+
+const nestedObjects = {
+    Person : {
+        Employee: {
+            designation : "Devloper"
+        },
+        Manager: {
+            designation : "Project Manager"
+        }
+    }
+}
+
+// console.log(nestedObjects.Person.Manager.designation);
+
+obj1 = {1:'R',2:'O'}
+obj2 = {3:'H',4:'I'}
+obj3 = {4:'T'}
+
+// obj4 = {obj1,obj2,obj3}
+// const obj4 = Object.assign({},obj1,obj2,obj3)
+const obj4 = {...obj1,...obj2,...obj3}
+// console.log(obj4);
+
+const emp = [
+    {
+        name : "Y",
+        dept : "IT"
+    },
+    {
+        name : "A",
+        dept : "HR"
+    },
+    {
+        name : "S",
+        dept : "Sales"
+    },
+    {
+        name : "H",
+        dept : "Purchasing"
+    }
+]
+
+// console.log(emp[0].dept);
+
+// console.log(Object.keys(bankAccount));
+// console.log(Object.values(bankAccount));
+// console.log(Object.entries(bankAccount));
+
+console.log(bankAccount.hasOwnProperty('email'));
+console.log(bankAccount.hasOwnProperty('balance'));
