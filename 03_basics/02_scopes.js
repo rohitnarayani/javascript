599be68685b093d88f1dd26b1e32b52b24018595
Rohit Narayani <rohitnarayani2005@gmail.com>
2026-05-19 18:10:01 +0000

    scopes of variable in js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,51 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+declaredAfterCall();
+
+function declaredAfterCall(){
+    console.log("You can call the function just before its declaration")
+}
+
+expressionFunction()
+const exp = function expressionFunction(){
+    console.log("You can not call expression function or hoisting just before the declaration");
+}
