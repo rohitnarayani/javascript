120472aa1f1fa2e0c316e04d28fc6cad8b521b19
Rohit Narayani <rohitnarayani2005@gmail.com>
2026-05-19 16:07:56 +0000

    Passing object and array in function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -18,5 +18,33 @@ function functionWithDefaultParameters(user = 'Unknow'){
     console.log(`${user} logged in successfully`);
     
 }
-console.log(isEligibleToVote(18));
-functionWithDefaultParameters("Raj");
+// console.log(isEligibleToVote(18));
+// functionWithDefaultParameters("Raj");
+
+function calculateCartPrice(...product){
+    return product;
+}
+
+// console.log(1,2,3);
+// console.log(1,2,3,4,5);
+// console.log(1,2,3,4,5,6,67,7);
+
+const user = {
+    email : "ceo@google.com",
+    password : "21212"
+}
+
+function handleObject(anyObject){
+    console.log(`Email: ${anyObject.email} and password: ${anyObject.password}`);
+}
+
+handleObject({
+    email : "ceo@google.com",
+    password : "21212"
+});
+
+function handleArray(arr){
+    return arr[0]*arr[1]
+}
+
+console.log(handleArray([10,20,30,40,50]))
